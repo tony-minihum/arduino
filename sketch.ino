a2cec72bfdaee5fb17efb434d49c80625ef866ab
Tony Minihum <toshiharuhot@yahoo.co.jp>
2012-08-09 23:20:50 +0900

    use HSV color space

--- sketch.ino
+++ sketch.ino
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*- */
 
-// LED sample
+// HSV Color Space
 
 const int	R    =  9;    // the pin LED connected
 const int	G    = 10;    // the pin LED connected
@@ -18,48 +18,87 @@ void setup()
 
 void loop()
 {
-  val = analogRead(0);
+  float val = analogRead(0) / 1024.f;
 
-  int max = 580 - 190;
-  int _min = 190;
-  Serial.print(max);
-  Serial.print(" ");
+  val = (val - 0.3f) * 2.2f; // 0 - 1 くらいになるように適当に補正
 
-  Serial.print(_min);
-  Serial.print(" ");
+  long l = (millis() / 5) % 1000;
+  //  val = sin( 2 * 3.14 * (l / 1000.f));
+  //  val = (val + 1.f) / 2.f;
+  val = l / 1000.f;
 
-  Serial.print(val);
-  Serial.print(" ");
-  val = val - _min;
-  if (val < 0) {
-    val = 0;
-  }
-  Serial.print(val);
-  Serial.print(" ");
-  if (val > max) {
-    val = max;
-  }
+  Serial.print("val=");
   Serial.print(val);
   Serial.print(" ");
 
-  if (val < max / 3) {
-    analogWrite(R, 255 * ((float)val/max));
-    analogWrite(G, 255 * (1-((float)val/max)));
-    analogWrite(B, 0);
-  } else if (val < max * 2 / 3) {
-    analogWrite(R, 0);
-    analogWrite(G, 255 * ((float)val/max));
-    analogWrite(B, 255 * (1-((float)val/max)));
-  } else if (val < max ) {
-    analogWrite(R, 255 * (1-((float)val/max)));
-    analogWrite(G, 0);
-    analogWrite(B, 255 * ((float)val/max));
-  } 
+  float r, g, b;  
 
-  Serial.print((float)val / max);
+  hueToRGB(val, r, g, b);
+
+  Serial.print("r=");
+  Serial.print(r);
   Serial.print(" ");
-  Serial.println(1-((float)val/max));
 
-  delay(10);
+  Serial.print("g=");
+  Serial.print(g);
+  Serial.print(" ");
+
+  Serial.print("b=");
+  Serial.print(b);
+  Serial.println(" ");
+
+  analogWrite(R, 10.f + 170.f * r * 0.5f);
+  analogWrite(G, 55.f + 200.f * g);
+  analogWrite(B, 1.f + 100.f * b * 0.1f);
+
+  //  delay(100);
+}
+
+void hueToRGB(float hue, float& r, float& g, float& b)
+{
+  float h = hue;
+  float s = 1.f;
+  float v = 1.f;
+
+  h *= 6.f; // 6 color sections
+  const int i = (int)h; //(int)floorf( h );
+  const float f = h - i;
+  const float p = v * ( 1.f - s );
+  const float q = v * ( 1.f - s * f );
+  const float t = v * ( 1.f - s * ( 1.f - f ) );
+
+  switch ( i )
+  {
+    case 0:
+      r = v;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = v;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = v;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = v;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = v;
+      break;
+    default:                // case 5:
+      r = v;
+      g = p;
+      b = q;
+      break;
+    }
 }
 
